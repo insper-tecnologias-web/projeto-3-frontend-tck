d3f1e16ddd0950d90498991ae2eca8574543ad93
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2025-05-30 18:58:57 -0300

    server.allowedHosts com URL do deploy do frontend

--- whatsapp2/vite.config.js
+++ whatsapp2/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['projeto-3-frontend-whatsapp2.onrender.com'],
+  },
 })
